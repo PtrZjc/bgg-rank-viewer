98fd1d0fb7d4c466e48cdada6d3217dfdaf3608a
Piotr Zajac <ptr.zjc@gmail.com>
2025-02-16 15:09:36 +0100

    Configure tailwing & DaisyUI

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({command}) => ({
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+    publicDir: command === "build" ? false : "public" // Handled in package.json to avoid copying /data folder
+}))
